d3b7c514b800268500c86c6c7ade718f3a5e87a4
Mohammad Saifur Rahman <52127126+saifposinc@users.noreply.github.com>
2020-10-17 23:41:34 +0200

    Update app.js
    Declare new event object, define object, call the object

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
+<<<<<<< HEAD
+const mod = require('./mod');
+const EventEmitter = require('events');
+const eventEmitter = new EventEmitter();
+console.log(mod);
+console.log(mod.sum(32,5));
+console.log(mod.PI);
+console.log(new mod.someMathObject());
+eventEmitter.on('objectName',(num1,num2)=>{
+	console.log(num1+num2);
+});
+eventEmitter.emit('objectName',12,5);
+=======
 const sumFunction = require('./mod');
 console.log(sumFunction);
 console.log(sumFunction(32,5));
 console.log(sumFunction.PI);
 console.log(sumFunction.someMathObject);
+>>>>>>> parent of 771ceae... Groupinmg the exports
