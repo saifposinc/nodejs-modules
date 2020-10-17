cafd87e94f5453e0ed1b85eb325fef5c15cfc096
Mohammad Saifur Rahman <52127126+saifposinc@users.noreply.github.com>
2020-10-17 23:12:24 +0200

    Multiple exports

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 const sumFunction = require('./mod');
 console.log(sumFunction);
 console.log(sumFunction(32,5));
+console.log(sumFunction.PI);
+console.log(sumFunction.someMathObject);
--- mod.js
+++ mod.js
@@ -1,2 +1,10 @@
 const sum = (num1,num2) => num1+num2;
 module.exports = sum;
+const PI = 3.14;
+class someMathObject{
+	constructor(){
+		console.log('object created');
+	}
+}
+module.exports.PI = PI;
+module.exports.someMathObject = someMathObject;
