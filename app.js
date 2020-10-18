eb2dd70130d055d560c3a61f4ee437ea6a8e4b0f
Mohammad Saifur Rahman <52127126+saifposinc@users.noreply.github.com>
2020-10-18 02:05:54 +0200

    Update app.js
    Working with readline module

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const mod = require('./mod');
 const EventEmitter = require('events');
-const eventEmitter = new EventEmitter();
+const readline = require('readline');
 console.log(mod);
 console.log(mod.sum(32,5));
 console.log(mod.PI);
 console.log(new mod.someMathObject());
+const eventEmitter = new EventEmitter();
 eventEmitter.on('objectName',(num1,num2)=>{
 	console.log(num1+num2);
 });
@@ -23,3 +24,28 @@ ed.on('name',()=>{
 	console.log('My name is '+ ed.name);
 });
 ed.emit('name');
+const rl = readline.createInterface({input: process.stdin,
+                                    output: process.stdout});
+let num1 = Math.floor((Math.random()*10)+1);
+let num2 = Math.floor((Math.random()*10)+1);
+let answer = num1+num2;
+rl.question('what is '+num1+' + '+num2+' : \n',(userInput)=>{
+    if(userInput.trim() == answer){
+        rl.close();
+    }
+    else{
+        rl.setPrompt('incorrect, try again\n');
+        rl.prompt();
+        rl.on('line',(userInput)=>{
+            if(userInput.trim() == answer)
+                rl.close();
+            else{
+                rl.setPrompt('incorrect, try again\n');
+                rl.prompt();
+            }
+        });
+    }
+});
+rl.on('close',()=>{
+    console.log('correct');
+})
